99e14b932fbb659451418fd34fa1f07fe4fbc702
banboobee <qzh00033@nifty.ne.jp>
2023-08-05 17:29:26 +0900

    Reverted 6e5ffc0. Added timeout for BLERefreshStatus().

--- src/device/meter.ts
+++ src/device/meter.ts
@@ -266,7 +266,16 @@ export class Meter {
       this.errorLog(`${this.device.deviceType}: ${this.accessory.displayName} refreshStatus enableCloudService: ${this.device.enableCloudService}`);
     } else if (this.BLE) {
       this.platform.BLEQue.use(async () => {
-	await this.BLERefreshStatus();
+	return new Promise(async (resolve, reject) => {
+	  const timeout = setTimeout(() => {
+	    reject(new Error(`timed out of ${this.scanDuration+1} seconds.`));
+	  }, this.scanDuration * 1000 + 1000);
+	  await this.BLERefreshStatus();
+	  clearTimeout(timeout);
+	  resolve(true);
+	}).catch((e) => {
+	  this.errorLog(`${this.device.deviceType}: ${this.accessory.displayName} BLErefreshStatus: ${e}`);
+	})
       })
     } else if (this.OpenAPI && this.platform.config.credentials?.token) {
       await this.openAPIRefreshStatus();
@@ -282,6 +291,7 @@ export class Meter {
   async BLERefreshStatus(): Promise<void> {
     this.debugLog(`${this.device.deviceType}: ${this.accessory.displayName} BLERefreshStatus`);
     const switchbot = await this.platform.connectBLE();
+    let scaned = false;
     // Convert to BLE Address
     this.device.bleMac = this.device
       .deviceId!.match(/.{1,2}/g)!
@@ -291,7 +301,7 @@ export class Meter {
     this.getCustomBLEAddress(switchbot);
     // Start to monitor advertisement packets
     if (switchbot !== false) {
-      await switchbot
+      return await switchbot
         .startScan({
           model: 'T',
           id: this.device.bleMac,
@@ -299,6 +309,7 @@ export class Meter {
         .then(async () => {
           // Set an event hander
           switchbot.onadvertisement = async (ad: ad) => {
+	    scaned = true;
             this.debugLog(
               `${this.device.deviceType}: ${this.accessory.displayName} Config BLE Address: ${this.device.bleMac},` +
               ` BLE Address Found: ${ad.address}`,
@@ -332,7 +343,10 @@ export class Meter {
         })
         .then(async () => {
           // Stop to monitor
-          await this.stopScanning(switchbot);
+    	  if (scaned === false) {
+            this.debugLog(`${this.device.deviceType}: ${this.accessory.displayName} BLERefreshStatus failed to scan. Keeps last values.`);
+	  }
+	  return await this.stopScanning(switchbot);
         })
         .catch(async (e: any) => {
           this.apiError(e);
@@ -340,10 +354,10 @@ export class Meter {
             `${this.device.deviceType}: ${this.accessory.displayName} failed BLERefreshStatus with ${this.device.connectionType}` +
             ` Connection, Error Message: ${JSON.stringify(e.message)}`,
           );
-          await this.BLERefreshConnection(switchbot);
+          return await this.BLERefreshConnection(switchbot);
         });
     } else {
-      await this.BLERefreshConnection(switchbot);
+      return await this.BLERefreshConnection(switchbot);
     }
   }
 
@@ -521,7 +535,7 @@ export class Meter {
   }
 
   async stopScanning(switchbot: any) {
-    await switchbot.stopScan();
+    switchbot.stopScan();
     if (this.BLE_IsConnected) {
       await this.BLEparseStatus();
       await this.updateHomeKitCharacteristics();
@@ -543,7 +557,7 @@ export class Meter {
         };
         await sleep(10000);
         // Stop to monitor
-        await switchbot.stopScan();
+        switchbot.stopScan();
       })();
     }
   }
